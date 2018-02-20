f2dd1f38a91f89bfcf690680e9852b398ed4ecf2
AnthonyN617 <Anthonyn617@gmail.com>
2018-02-20 13:20:20 -0500

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public GameObject enemyDeath;
+    public GameObject missileExplosion;
+    public LayerMask mask;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 2.0f, mask);
+
+        for (int i = 0; i < hitColliders.Length; i++)
+        {
+            if (hitColliders[i] != null) ;
+            {
+                Instantiate(enemyDeath, hitColliders[i].transform.position, Quaternion.identity);
+                Destroy(hitColliders[i].gameObject);
+            }
+
+
+            Instantiate(missileExplosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        if (gameObject.tag == "barrier")
+        {
+
+        }
+    }
+}
